da5a942aaa054641cac1de50e2451e3ba0ad236b
Aukai Tirrell <aukai.tirrell@gmail.com>
2021-03-29 17:42:07 -0700

    add new layout

--- src/pages/drink-menu.js
+++ src/pages/drink-menu.js
@@ -8,7 +8,18 @@ const DrinkMenu = () => {
   return (
     <Layout>
       <SEO title="drink-menu" />
-      <h1>Signature Cock-Tails</h1>
+      <div className="drink-menu">
+        <h1 className="drink-menu__title">Signature Cock-Tails</h1>
+        <MenuItem
+          title="Love Language"
+          description="Vodka, Lavender syrup Lemon juice, Elderflower syrup, & Ginger Beer"
+        />
+      </div>
+      <MenuItem title="Jazzy FKN Honey" description="Bourgon, Honey, & Lemon" />
+      <MenuItem
+        title="Aries in a Glass"
+        description="Tequila & Grapefruit Juice"
+      />
     </Layout>
   )
 }
