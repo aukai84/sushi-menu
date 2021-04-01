39be6337b609b7fd614387d33f3d0f0db865d82e
Aukai Tirrell <aukai.tirrell@gmail.com>
2021-03-31 17:34:13 -0700

    update men u

--- src/pages/sushi-menu.js
+++ src/pages/sushi-menu.js
@@ -12,15 +12,23 @@ const SushiMenu = () => {
         <h1 className="menu__title">Sushi</h1>
         <MenuItem
           title={"Hamachi Nigiri"}
-          description="Fresh cut Hamachi nigiri sushi.  Lightly brushed with Shoyu with a touch of Wasabi"
+          description="Fresh cut Hamachi nigiri sushi.  Lightly brushed with shoyu with a touch of wasabi"
         />
         <MenuItem
           title={"Ahi Nigiri"}
-          description="Fresh cut Hamachi nigiri sushi.  Lightly brushed with Shoyu with a touch of Wasabi"
+          description="Fresh cut Ahi sushi.  Lightly brushed with shoyu with a touch of wasabi"
         />
         <MenuItem
           title={"Salmon Nigiri"}
-          description="Fresh cut Hamachi nigiri sushi.  Lightly brushed with Shoyu with a touch of Wasabi"
+          description="Fresh cut Salmon nigiri sushi.  Lightly brushed with shoyu with a touch of wasabi"
+        />
+        <MenuItem
+          title={"Jalapeno Hamachi"}
+          description=" thinly sliced hamachi set in yuzu soy topped with jalapeño and a little bit of cilantro in for added texture "
+        />
+        <MenuItem
+          title={"Hayami Wagyu Taco"}
+          description="Aomori wagyu cooked and served with tamanegi, topped with cilantro onion in our crispy taco shells."
         />
       </div>
     </Layout>
